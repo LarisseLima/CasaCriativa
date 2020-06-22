a2469640e6634007ee1deeba3b0308506bfa4ba8
Larisse Lima <larisse.lima2@gmail.com>
2020-06-22 14:54:55 -0300

    Projeto Final

--- db.js
+++ db.js
@@ -1,63 +1,55 @@
-const sqlite = require('sqlite3').verbose()
-const db = new sqlite.Database("./ws.db")
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('./CASACRIATIVA.db')
 
-db.serialize(function () 
-{
-  // Criar a Tabela
-  db.run(`
-    CREATE TABLE IF NOT EXISTS ideas (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      image TEXT, 
-      title TEXT,
-      category TEXT, 
-      description TEXT,
-      link TEXT
-    );
-  `)
+db.serialize(function () {
+  // criar a tabela
+  // db.run(`
+  //       CREATE TABLE IF NOT EXISTS ideas(
+  //           id INTEGER PRIMARY KEY AUTOINCREMENT,
+  //           image TEXT,
+  //           title TEXT,
+  //           category TEXT,
+  //           description TEXT,
+  //           link TEXT
+  //       );
+  //   `)
+  //inserir dados na tabela
+  const query = `
+  INSERT INTO ideas(
+      image,
+      title,
+      category,
+      description,
+      link
+  ) VALUES (?,?,?,?,?);
+   `
+  const values = [
+    "https://image.flaticon.com/icons/svg/2964/2964888.svg",
+    "Ler livros",
+    "Leitura",
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure alias, repellat dolore perferendis ut ipsam adipisci corporis ab. Consectetur incidunt veniam alias soluta nulla vel commodi a iusto rerum sunt.",
+    "https://www.amazon.com.br/b?node=6311441011"
+  ]
 
-  // Inserir Dados
+  db.run(query, values, function (err) {
+    if (err) return console.log(err)
 
-    const query = (`
-      INSERT INTO ideas(
-        image,
-        title,
-        category,
-        description,
-        link
-      ) values (?,?,?,?,?);
-    `)
-  
-    const values = [
-      "https://image.flaticon.com/icons/svg/2758/2758767.svg",
-      "Cursos de Programação",
-      "Estudo",
-      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui dolorem accusamus eos dolor ipsum est, aliquam totam ea, error tempora fugit id nisi quo eligendi nesciunt quibusdam eum eius officiis!",
-      "/",
-    ]
+    console.log(this)
+  })
 
-  // db.run(query, values, function (err){
-  //   if (err) return console.log(err)
-  //   console.log(this)
+  // Deletar dados da tabela
+  // db.run(`DELETE FROM ideas WHERE id = ?`, [1], function(err){
+  //     if (err) return console.log(err)
+
+  //     console.log("deletei", this)
   // })
 
-  // Consultar Dados
+  // consultar dados da tabela
+  // db.all(`SELECT * FROM ideas`, function(err, rows){
+  //     if (err) return console.log(err)
 
-    db.all(`SELECT * FROM ideas`, function(err, rows){
-      if(err) return console.log(err)
-
-        console.log(rows)
-    })
-
-  // Deletar Dados
-
-    // db.run(`DELETE FROM ideas WHERE id = ?`, [1], function(err){
-    //   if(err) return console.log(err)
-
-    //   console.log(this)
-    // })
-    //db.run(`DELETE FROM ideas`)
-    
+  //     console.log(rows)
+  //})
 })
-
 
 module.exports = db
--- script.js
+++ script.js
@@ -1,18 +1,42 @@
-
-function onOff(tag) {
+function onOff() {
 
   document
-    .querySelector(tag)
+    .querySelector("#modal")
     .classList
     .toggle("hide")
 
-    document
-      .querySelector("body")
-      .classList
-      .toggle("hideScroll")
+  document
+    .querySelector("body")
+    .classList
+    .toggle("hideScroll")
 
-    document
-      .querySelector(tag)
-      .classList
-      .toggle("addScroll")
+  document
+    .querySelector("#modal")
+    .classList
+    .toggle("addScroll")
 }
+
+function checkFields(event) {
+  const valuesToCheck = [
+    "title",
+    "image",
+    "category",
+    "description",
+    "link"
+  ]
+
+  const isEmpty = valuesToCheck.find(function (value) {
+    const checkIfItsString = typeof event.target[value].value === "string"
+    const checkIfItsEmpty = !event.target[value].value.trim()
+    if (checkIfItsString && checkIfItsEmpty) return true
+  })
+
+  if (isEmpty) {
+    event.preventDefault()
+    alert("Por favor, preencha todos os campos")
+  }
+
+}
+
+
+
