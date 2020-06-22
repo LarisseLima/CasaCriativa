78dd390b15ca848170d2a3ef239cc1340466a16f
Larisse Lima <larisse.lima2@gmail.com>
2020-06-22 10:29:30 -0300

    Adiciona Nunjucks

--- server.js
+++ server.js
@@ -5,11 +5,29 @@ const server = express()
 // Configurar arquivos estáticos ( css, scripts , imagens)
 server.use(express.static("public"))
 
+//configuração do nunjucks
+const nunjucks = require("nunjucks");
+nunjucks.configure("views", {
+    express: server,
+    noCache: true,
+})
+
+
 // Criei uma rota /
 // E capturo o pedido do cliente para responder
 server.get('/', function (req, res) {
-    return res.sendFile(__dirname + "/index.html")
+    const reversedIdeas = [...rows].reverse();
+    let lastIdeas = [];
+    for (let idea of reversedIdeas) {
+        if (lastIdeas.length < 2) {
+            lastIdeas.push(idea)
+        }
+    }
 
+    return res.render('index.html', { ideas: lastIdeas });
+})
+server.get('/', function (req, res) {
+    return res.render("ideias.html")
 })
 
 // ligar o servidor na porta 3000
